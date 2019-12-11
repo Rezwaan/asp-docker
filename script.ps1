cee81ba654124e37679a75ddc5d0ec43a090ac0c
Rehman <attiq.ur.rehman@spglobal.com>
2019-12-11 18:38:54 +0500

    changes at 0638

--- script.ps1
+++ script.ps1
@@ -1,5 +1,6 @@
 # Writes an error to build summary and to log in red text
 #Write-Host  "##vso[task.LogIssue type=error;]This is the error and we need to initmate the developer team for this"
 #exit 1
-$vers = $PSVersionTable
-Write-Host "Curren version is $vers "
+#$vers = $PSVersionTable
+#Write-Host "Curren version is $vers "
+Get-Module -ListAvailable -Name Azure -Refresh
