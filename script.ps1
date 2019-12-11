d91cb24290739c0dc7f0f9823ddc4ad2af6aebfb
Rehman <attiq.ur.rehman@spglobal.com>
2019-12-11 17:44:31 +0500

    changes at 0544

--- script.ps1
+++ script.ps1
@@ -1,3 +1,5 @@
 # Writes an error to build summary and to log in red text
-Write-Host  "##vso[task.LogIssue type=error;]This is the error and we need to initmate the developer team for this"
-exit 1
+#Write-Host  "##vso[task.LogIssue type=error;]This is the error and we need to initmate the developer team for this"
+#exit 1
+$vers = $PSVersionTable
+Write-Host "Curren version is $vers "
